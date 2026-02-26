a7de701b4ef6ab9ee131904670aa5b29094e398f
Phat <phat.chauu26@gmail.com>
2026-02-25 18:24:47 -0800

    new provider on terraform

---
+++ infra/terraform/providers.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "cloud-notes-terraform-state-bucket"
+    key            = "state/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "cloud-notes-terraform-state-lock"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+  default_tags {
+    tags = {
+      Project     = "Cloud-Notes-App"
+      Environment = var.environment
+      ManagedBy   = "Terraform"
+    }
+  }
+}
